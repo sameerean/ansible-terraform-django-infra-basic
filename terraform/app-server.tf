e1bad7cd5a9501293e0f47b524e417cc20421ba9
Shameer Kunjumohamed <sameerean@gmail.com>
2019-06-29 19:40:21 +0000

    Firewall settings added

--- terraform/app-server.tf
+++ terraform/app-server.tf
@@ -9,7 +9,6 @@ resource "digitalocean_droplet" "app-server" {
 
     ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
     //ssh_keys = ["${var.ssh_fingerprint}"]
-
     
 }
 
@@ -24,3 +23,97 @@ resource "digitalocean_ssh_key" "default" {
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
+resource "digitalocean_firewall" "app-server" {
+  name = "only-22-80-and-443"
+
+  // 22, 80, 8000, 8200, 8300, 9001
+
+  droplet_ids = ["${digitalocean_droplet.app-server.id}"]
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "22"
+      //source_addresses   = ["192.168.1.0/24", "2002:1:2::/48"]
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "80"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "8000"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "8200"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "8300"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "9001"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "443"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "icmp"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "tcp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "udp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "icmp"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  # http/https connection
+  outbound_rule  {
+      protocol                       = "tcp"
+      port_range                     = "80"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+
+    #   destination_addresses          = ["${var.allowed_outbound_http_addresses}"]
+    #   destination_tags               = ["${var.allowed_outbound_http_tags}"]
+    #   destination_droplet_ids        = ["${var.allowed_outbound_http_droplet_ids}"]
+    #   destination_load_balancer_uids = ["${var.allowed_outbound_http_load_balancer_uids}"]
+    }
+    outbound_rule {
+      protocol                       = "tcp"
+      port_range                     = "443"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+    #   destination_addresses          = ["${var.allowed_outbound_https_addresses}"]
+    #   destination_tags               = ["${var.allowed_outbound_https_tags}"]
+    #   destination_droplet_ids        = ["${var.allowed_outbound_https_droplet_ids}"]
+    #   destination_load_balancer_uids = ["${var.allowed_outbound_https_load_balancer_uids}"]
+    }
+  
+}
